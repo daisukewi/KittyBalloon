e1cb67d82c5549493b86229ba825e44dad61085e
daisukewi <d.flamencog@gmail.com>
2014-06-15 00:32:21 +0200

    Add bouncingness with the platforms and other players/enemies

---
+++ Assets/KittyBalloon/Scripts/Controller/CollisionController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionController: MonoBehaviour {
+
+    Transform GetParent()
+    {
+        return gameObject.transform.parent;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.tag == "Balloons")
+        {
+            coll.collider.transform.parent.SendMessage("TakeDamage", 1.0f);
+        }
+
+        Vector2 collNormal = ((ContactPoint2D)coll.contacts.GetValue(0)).normal;
+
+        if (collNormal.y < 1.0f)
+        {
+            Vector2 reflection = coll.relativeVelocity - 2 * Vector2.Dot(coll.relativeVelocity, collNormal) * collNormal;
+            rigidbody2D.velocity = reflection.normalized;
+        }
+    }
+}
