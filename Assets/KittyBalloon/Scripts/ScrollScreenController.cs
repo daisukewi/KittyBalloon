d716f0a2a1d151ed2987bf58529e6b8b4f6010c5
daisukewi <d.flamencog@gmail.com>
2014-06-14 22:13:58 +0200

    Player respawns on the other side of the screen when gets out

---
+++ Assets/KittyBalloon/Scripts/ScrollScreenController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollScreenController : MonoBehaviour {
+
+    Transform GetParent()
+    {
+        return gameObject.transform.parent;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void OnBecameInvisible()
+    {
+        Vector2 newPos = GetParent().transform.position;
+        newPos.x = -newPos.x + (newPos.x > 0 ? 0.1f : -0.1f);
+
+        GetParent().transform.position = newPos;
+    }
+}
