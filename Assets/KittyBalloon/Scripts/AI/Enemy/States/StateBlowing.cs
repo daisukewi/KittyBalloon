d4b0cc2c565223a440bbbecba537f0bee7a67b8f
exAntares <luis_andres81@hotmail.com>
2014-06-14 23:53:35 +0200

    Enemy New Animations, states

--- Assets/KittyBalloon/Scripts/AI/Enemy/States/StateBlowing.cs
+++ Assets/KittyBalloon/Scripts/AI/Enemy/States/StateBlowing.cs
@@ -3,14 +3,49 @@ using System.Collections;
 
 public class StateBlowing : StateBase
 {
+    public bool CanBlow = true;
+    public float BlowCooldown = 10.0f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public override void BeginState()
+    {
+        if(CanBlow)
+        {
+            StartBlowing();
+        }
+    }
+
+    public override void EndState()
+    {
+        //Debug.Log ("StateBase EndState");
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void StartBlowing()
+    {
+        Invoke("OnBlowFinished", 3.0f);
+    }
+
+    void OnBlowFinished()
+    {
+        StartBlowingCooldown();
+        if(CanGoToState("Flying"))
+        {
+            GoToState("Flying");
+        }
+    }
+
+    void StartBlowingCooldown()
+    {
+        CanBlow = false;
+        Invoke("StopBlowingCooldown", BlowCooldown);
+    }
+
+    void StopBlowingCooldown()
+    {
+        CanBlow = true;
+    }
 }
