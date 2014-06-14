84b4028f43f5745887ede3331f114214b8f167c9
exAntares <luis_andres81@hotmail.com>
2014-06-15 00:36:29 +0200

    State Flying

--- Assets/KittyBalloon/Scripts/AI/Enemy/States/StateFlying.cs
+++ Assets/KittyBalloon/Scripts/AI/Enemy/States/StateFlying.cs
@@ -3,14 +3,32 @@ using System.Collections;
 
 public class StateFlying : StateBase
 {
+    public float MinVelocity = 20.0f;
+    public float MaxVelocity = 25.0f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    public Vector2 Direction;
+
+    override protected void InitState()
+    {
+        base.InitState();
+        InvokeRepeating("ChangeDirection", 0.1f, 2.0f);
+
+    }
+
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate ()
+    {
+        if(enabled)
+        {
+            // Add a vertical force to the player.
+            rigidbody2D.AddForce(Direction);
+        }
 	}
+
+    void ChangeDirection()
+    {
+        Direction = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(1.0f, 1.0f));
+
+        Direction *= Random.Range(MinVelocity, MaxVelocity);
+    }
 }
