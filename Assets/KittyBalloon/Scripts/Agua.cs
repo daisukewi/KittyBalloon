84ca22005fc6b0ed9f87d90a42fdbd6b8ffd828a
exAntares <luis_andres81@hotmail.com>
2014-06-15 04:20:05 +0200

    Nuevo rio, plataforma 01, cambio de resolucion

---
+++ Assets/KittyBalloon/Scripts/Agua.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agua : MonoBehaviour {
+
+    public float DeltaDistance = 1.0f;
+    public float VerticalSpeed = 0.1f;
+    public float HorizontalSpeed = 0.01f;
+
+    private float RandStart = 1.0f;
+
+    private Vector3 Begin;
+    private Vector3 End;
+
+    void Start()
+    {
+        Begin = transform.parent.Find("Begin").position;
+        End = transform.parent.Find("End").position;
+
+        RandStart = Random.Range(0.0f,100.0f);
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Vector3 tmp = transform.position;
+        tmp.y = Begin.y + (DeltaDistance * Mathf.Sin(VerticalSpeed * (RandStart + Time.frameCount)));
+        tmp.x -= HorizontalSpeed;
+
+        if (Mathf.Abs(End.x - tmp.x) <= 0.01)
+        {
+            tmp = Begin;
+        }
+
+        transform.position = tmp;
+	}
+}
